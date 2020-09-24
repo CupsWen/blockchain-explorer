63f55749283d8ce0001f28eb0f857ee1e700c1c7
CupsWen <1191567189@qq.com>
2020-09-24 21:38:09 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,20 @@
+#
+#    SPDX-License-Identifier: Apache-2.0
+#
+
+#!/bin/bash
+#
+#Redirecting console.log to log file.
+#Please visit ./logs/app to view the application logs and visit the ./logs/db to view the Database logs and visit the ./log/console for the console.log
+# Log rotating for every 7 days.
+
+echo "************************************************************************************"
+echo "**************************** Hyperledger Explorer **********************************"
+echo "************************************************************************************"
+
+export LOG_LEVEL_APP=debug
+export LOG_LEVEL_DB=debug
+export LOG_LEVEL_CONSOLE=info
+export LOG_CONSOLE_STDOUT=true
+export DISCOVERY_AS_LOCALHOST=false
+node main.js name - hyperledger-explorer &
